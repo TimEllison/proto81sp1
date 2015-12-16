04e4a9223f26462b7cd18f049afde7abebed5a29
TimEllison <tellison@captechconsulting.com>
2015-12-16 12:19:05 -0500

    leads sample

---
+++ Proto.Model/Dao/LeadFormRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proto.Model.Dao
+{
+    public class LeadFormRepository
+    {
+        public void AddLead(Lead leadToAdd)
+        {
+            using (var context = new LeadFormsEntities())
+            {
+                leadToAdd.LeadTypeId = 1;
+                context.Leads.Add(leadToAdd);
+                context.SaveChanges();
+            }
+        }
+    }
+}
